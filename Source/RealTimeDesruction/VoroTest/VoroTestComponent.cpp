fdc5ae8117287b9724f86578d73ea74ae90a7916
dkeke <ano0717@gmail.com>
2024-11-09 19:02:26 +0900

    보로노이 생성 컴포넌트 작성

---
+++ Source/RealTimeDesruction/VoroTest/VoroTestComponent.cpp
@@ -0,0 +1,102 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VoroTestComponent.h"
+
+// For Voronoi Cells
+static TMap<int32, FColor> ColorMap = {
+	{0, FColor::Red},
+	{1, FColor::Green},
+	{2, FColor::Blue},
+	{3, FColor::Yellow},
+	{4, FColor::Cyan},
+	{5, FColor::Magenta},
+	{6, FColor::Turquoise},
+	{7, FColor::Silver}
+};
+
+// Sets default values for this component's properties
+UVoroTestComponent::UVoroTestComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UVoroTestComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	TetMeshComponent = GetOwner()->FindComponentByClass<UTetMeshGenerateComponent>();
+
+	if (TetMeshComponent != nullptr)
+	{
+		GetOwner()->FindComponentByClass<UStaticMeshComponent>()->SetVisibility(bMeshVisibility);
+
+		TetMeshComponent->Graph;
+
+		Sites = getRandomVoronoiSites(TetMeshComponent->TetMeshVertices.Num());
+
+		if (bUseCVT)
+		{
+			CVT CVT_inst;
+
+			CVT_inst.SetVertices(TetMeshComponent->TetMeshVertices);
+			CVT_inst.SetVoronoiSites(Sites);
+			CVT_inst.Lloyd_Algo();
+			Sites = CVT_inst.Sites;
+			Region = CVT_inst.Region;
+			VisualizeVertices();
+		}
+
+		// Gen Final Voro
+		
+	}
+}
+
+
+// Called every frame
+void UVoroTestComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+TArray<uint32> UVoroTestComponent::getRandomVoronoiSites(const int32 VeticesSize)
+{
+	TArray<uint32> VoronoiSites;
+	TSet<uint32> SelectedIndices;
+
+	SiteNum = FMath::Min(SiteNum, VeticesSize);
+
+	while (SelectedIndices.Num() < SiteNum)
+	{
+		// ·£´ý ÀÎµ¦½º »ý¼º
+		uint32 RandomIndex = FMath::RandRange(0, VeticesSize - 1);
+		SelectedIndices.Add(RandomIndex);
+	}
+	VoronoiSites = SelectedIndices.Array();
+	VoronoiSites.Sort();
+
+	return VoronoiSites;
+}
+
+void UVoroTestComponent::VisualizeVertices()
+{
+	for (int32 i = 0; i < TetMeshComponent->TetMeshVertices.Num(); i++)
+	{
+		FVector WorldPosition = GetOwner()->GetActorTransform().TransformPosition(static_cast<FVector>(TetMeshComponent->TetMeshVertices[i]));
+		int32 RegionOfVertex = Region[i];
+
+		if (i == Sites[RegionOfVertex])
+			DrawDebugPoint(GetWorld(), WorldPosition, 15.0f, FColor::Black, true, -1.0f, 0);
+		else
+			DrawDebugPoint(GetWorld(), WorldPosition, 15.0f, ColorMap[RegionOfVertex % ColorMap.Num()], true, -1.0f, 0);
+	}
+}
---
+++ Source/RealTimeDesruction/VoroTest/VoroTestComponent.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "../TetMesh/TetMeshGenerateComponent.h"
+#include "../CVT/CVT.h"
+#include "../DistanceCalculate/DistanceCalculate.h"
+#include "VoroTestComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class REALTIMEDESRUCTION_API UVoroTestComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UVoroTestComponent();
+
+	UPROPERTY(EditAnywhere, Category = "Dataflow")
+	bool bUseCVT;
+	UPROPERTY(EditAnywhere, Category = "Dataflow")
+	bool bMeshVisibility;
+	UPROPERTY(EditAnywhere, Category = "Dataflow", meta = (ClampMin = "1"))
+	int32 SiteNum = 10;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UTetMeshGenerateComponent* TetMeshComponent = nullptr;
+	TArray<uint32> Sites;
+	TArray<uint32> Region;
+
+	TArray<uint32> getRandomVoronoiSites(const int32 VeticesSize);
+	void VisualizeVertices();
+};
